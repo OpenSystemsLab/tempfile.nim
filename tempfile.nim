4c819076a4a14afd133ffe51454ab7c9bcdd1b3f
root <me@huy.im>
2018-12-02 22:24:05 +0700

    use a more trustable `getTempDir` proc, rename `mktemp` to `mktempUnsafe` [refs #9]

--- tempfile.nim
+++ tempfile.nim
@@ -3,19 +3,47 @@
 ##
 ## This module is impressed by Python's `tempfile` module
 
-import os
-import math
-
-# Since random number generator was moved to separate module:
-when not compiles(randomize()):
-  import random
+import os, math, random
 
 const
   MAX_RETRIES = 9999
   CHARSET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 randomize()
-proc mktemp*(prefix = "tmp", suffix = "", dir = "", len = 8): string {.deprecated.} =
+
+
+proc getTempDir(): string =
+  when defined(tempDir):
+    const tempDir {.strdefine.}: string = nil
+    return tempDir
+
+  # Lines bellow are forked of Boost's filesystem library
+  # https://github.com/boostorg/filesystem/blob/0703e62a317c6a8ec63fd87c556d0b8e8ac956df/src/operations.cpp#L1863
+
+  when defined(posix):
+    for env in ["TMP", "TEMP", "TMPDIR", "TEMPDIR"]:
+      result = getenv(env)
+      if result.len != 0 and result.dirExists:
+        return result
+    when defined(android):
+      let default = "/data/local/tmp"
+    else:
+      let default = "/tmp"
+    if default.dirExists:
+      return default
+  else:
+    let envList = ["TMP", "TEMP", "LOCALAPPDATA", "USERPROFILE"]
+    for i in 0..<envList.len:
+      result = getenv(envList[i])
+      if result.len != 0:
+        if i >= 2:
+          result = result.joinPath("Temp")
+        if result.dirExists:
+          return result
+  raise newException(IOError, "unable to find temp directory")
+
+
+proc mktempUnsafe*(prefix = "tmp", suffix = "", dir = "", len = 8): string =
   ## Returns a unique temporary file name. The file is not created.
 
   var name = newString(len)
@@ -37,7 +65,7 @@ proc mkstemp*(prefix = "tmp", suffix = "", dir = "", mode = fmRead): tuple[fd: F
   var name: string
   var file: File
   for x in 0..MAX_RETRIES:
-    name = mktemp(prefix, suffix, dir)
+    name = mktempUnsafe(prefix, suffix, dir)
     try:
       # force create file
       file = name.open(fmWrite)
@@ -57,7 +85,7 @@ proc mkdtemp*(prefix = "tmp", suffix = "", dir = ""): string =
   ## Returns value is the pathname of the directory.
   var path: string
   for x in 0..MAX_RETRIES:
-    path = mktemp(prefix, suffix, dir)
+    path = mktempUnsafe(prefix, suffix, dir)
     try:
       when declared(existsOrCreateDir):
         # Nim 0.15.3 or later
